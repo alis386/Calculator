f441561d3169ffe92d0c74e7a4ff256d485d2449
Alicja Moczala <alis386@gmail.com>
2017-07-11 11:56:54 +0200

    Separate numbers and operators

--- src/calculator/ExpressionSplitter.java
+++ src/calculator/ExpressionSplitter.java
@@ -3,12 +3,44 @@ package calculator;
 public class ExpressionSplitter {
 
     String expression;
+    int position = 0;
 
     public ExpressionSplitter(String expression) {
         this.expression = expression;
     }
 
     public String getElement() {
-        return "Ala";
+        if (position >= expression.length()) {
+            return null;
+        }
+
+        Character character = expression.charAt(position++);
+
+        if (!isNumber(character)) {
+            return character.toString();
+        }
+
+        String number = character.toString();
+
+        while (position < expression.length()) {
+            character = expression.charAt(position);
+            if (isNumber(character) == true) {
+                number = number + character.toString();
+                position++;
+            } else {
+                break;
+            }
+        }
+
+        return number;
     }
+
+    private boolean isNumber(Character character) {
+        if ((character >= '0' && character <= '9') || character == '.') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
