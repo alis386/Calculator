9d657d65e453f0f12a24bed3a39dc35f512b416c
Alicja Moczala <alis386@gmail.com>
2017-07-15 16:23:03 +0200

    Completed Solver

--- src/calculator/Solver.java
+++ src/calculator/Solver.java
@@ -1,17 +1,56 @@
 package calculator;
 
+import java.util.Stack;
+
 public class Solver {
 
+    private Stack<Double> stack = new Stack();
+
     public void push(String element) {
-        // ...
-        
-        System.out.println(element);
+        if (isNumber(element)) {
+            stack.push(Double.parseDouble(element));
+            return;
+        }
+
+        double second = stack.pop();
+        double first = stack.pop();
+        double result = 0;
+
+        switch (element.charAt(0)) {
+            case '+':
+                result = first + second;
+                break;
+            case '-':
+                result = first - second;
+                break;
+            case '*':
+                result = first * second;
+                break;
+            case '/':
+                result = first / second;
+                break;
+            case '^':
+                result = Math.pow(first, second);
+                break;
+        }
+
+        stack.push(result);
     }
 
     public double getResult() {
-        // ..
-        
-        return 0;
+        if (stack.empty()) {
+            return 0;
+        } else {
+            return stack.peek();
+        }
     }
 
+    private boolean isNumber(String element) {
+        try {
+            Double.parseDouble(element);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
